53b82492e87bbdc9bfc890ad4e5bb33d9bc16436
Alexander Morland <alek.morland@gmail.com>
2016-02-06 23:07:26 +0100

    Day Seven part 2

--- advent/seven/solver_seven.py
+++ advent/seven/solver_seven.py
@@ -7,8 +7,4 @@ print "*********************"
 print ""
 print "The output of a is [%d]" % solver.check("a")
 print ""
-# print "*********************"
-# print ""
-# print "You need [%d] feet of ribbon" % how_much_ribbon_in_bag(bag)
-# print ""
 print "*********************"
